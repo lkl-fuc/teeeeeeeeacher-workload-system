cfa4b50d14c2178965020e3db0b77697e3a02cea
刘柯良 <1439094669@qq.com>
2026-03-24 15:29:18 +0800

    init project

---
+++ backend/teacher-workload-backend/src/test/java/com/example/teacherworkloadbackend/TeacherWorkloadBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teacherworkloadbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherWorkloadBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
